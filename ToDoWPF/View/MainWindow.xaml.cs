ca1a0da7ba29a3a2eb82e24c8a54fc3341fb8856
Enak1n <masj963@gmail.com>
2023-04-14 18:12:50 +0300

    Add items in ListBox and buttons

--- ToDoWPF/View/MainWindow.xaml.cs
+++ ToDoWPF/View/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,27 +27,80 @@ namespace ToDoWPF.View
 
         private void AddNoteButton_Click(object sender, RoutedEventArgs e)
         {
-            TextBlock textBlock = new TextBlock() { FontSize = 20, Text = AddNoteTextBox.Text, Foreground=Brushes.Black};
-            textBlock.FontWeight = FontWeights.SemiBold;
-            StackPanel stackPanel = new StackPanel();
-
-            Border border = new Border
+            if (AddNoteTextBox.Text == "" || AddNoteTextBox.Text.Length > 40)
             {
-                Background = Brushes.White,
-                BorderBrush = Brushes.LightGray,
-                BorderThickness = new Thickness(3),
-                Margin = new Thickness(5, 10, 0, 0),
-                CornerRadius = new CornerRadius(5),
-                HorizontalAlignment = HorizontalAlignment.Left,
-                Width = 350,
-                Height = 40,
-                Child = textBlock
-            };
+                MessageBox.Show("Length is so long!");
+            }
+            else
+            {
+                Image editImage = new Image();
+                editImage.Source = new BitmapImage(new Uri("/Images/edit_64px.ico", UriKind.Relative));
 
-            border.Child = textBlock;
-            stackPanel.Children.Add(border);
-            NotesGroupBox.Items.Add(stackPanel);
-            AddNoteTextBox.Clear();
+                Image deleteImage = new Image();
+                deleteImage.Source = new BitmapImage(new Uri("/Images/delete_64px.ico", UriKind.Relative));
+
+                TextBlock textBlock = new TextBlock() { FontSize = 20, Text = AddNoteTextBox.Text, Foreground = Brushes.Black };
+                textBlock.FontWeight = FontWeights.SemiBold;
+                StackPanel stackPanel = new StackPanel();
+
+                Button editButton = new Button()
+                {
+                    Content = editImage,
+                    Background = Brushes.White,
+                    Margin = new Thickness(5, 10, 0, 0),
+                };
+
+                editButton.Click += new RoutedEventHandler(EditButton_Click);
+
+                Button deleteButton = new Button()
+                {
+                    Content = deleteImage,
+                    Background = Brushes.White,
+                    Margin = new Thickness(5, 10, 0, 0),
+                };
+
+                deleteButton.Click += new RoutedEventHandler(DeleteButton_Click);
+
+                CheckBox finishTask = new CheckBox()
+                {
+                    BorderThickness = new Thickness(3),
+                    BorderBrush = Brushes.Black,
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Bottom,
+                    Content = "Task is completed"
+                };
+
+                Border border = new Border
+                {
+                    Background = Brushes.White,
+                    BorderBrush = Brushes.LightGray,
+                    BorderThickness = new Thickness(3),
+                    Margin = new Thickness(5, 10, 0, 0),
+                    CornerRadius = new CornerRadius(5),
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    Width = 700,
+                    Height = 40,
+                    Child = textBlock
+                };
+
+
+                stackPanel.Children.Add(border);
+                stackPanel.Children.Add(finishTask);
+                stackPanel.Children.Add(editButton);
+                stackPanel.Children.Add(deleteButton);
+                NotesGroupBox.Items.Add(stackPanel);
+                AddNoteTextBox.Clear();
+            }
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Edit");
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Delete");
         }
     }
 }
